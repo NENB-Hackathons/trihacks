8e39276c2b330c80dbf3287ae71710425e388c89
Michael Zheng <zhengm58@gmail.com>
2022-10-07 21:55:31 -0600

    Init scraper code

---
+++ scraper.py
@@ -0,0 +1,19 @@
+# Take `destination`, `origin` and `parcel size` as an argument and scrape websites for shipping times and prices
+
+import requests
+
+# Fedex API - refer to https://developer.fedex.com/api/en-us/catalog/rate/v1/docs.html
+url = "https://apis-sandbox.fedex.com/rate/v1/rates/quotes"
+
+payload = input # 'input' refers to JSON Payload
+headers = {
+    'Content-Type': "application/json",
+    'X-locale': "en_US",
+    'Authorization': "Bearer "
+    }
+
+response = requests.post(url, data=payload, headers=headers)
+
+print(response.text)
+
+# UPS API - refer to https://www.ups.com/upsdeveloperkit?loc=en_US
