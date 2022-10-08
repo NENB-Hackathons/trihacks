a83bd4b433438d360d57bee938e8615e311240b0
Brandon Kim <105524304+ENC0D3D-WasTaken@users.noreply.github.com>
2022-10-08 07:11:57 -0600

    minor fixes

--- scraper.py
+++ scraper.py
@@ -5,27 +5,27 @@ import requests
 # Fedex API - refer to https://developer.fedex.com/api/en-us/catalog/rate/v1/docs.html
 fexEx_url = "https://apis-sandbox.fedex.com/rate/v1/rates/quotes"
 
-payload = input # 'input' refers to JSON Payload
+fedEx_payload = input # 'input' refers to JSON Payload
 headers = {
     'Content-Type': "application/json",
     'X-locale': "en_US",
     'Authorization': "Bearer "
     }
 
-response = requests.post(fexEx_url, data=payload, headers=headers)
+response = requests.post(fexEx_url, data=fedEx_payload, headers=headers)
 
 print(response.text)
 
 # UPS API - refer to https://www.ups.com/upsdeveloperkit?loc=en_US
 ups_url = "blank for now"
 
-payload = input # 'input' refers to JSON Payload
+ups_payload = input # 'input' refers to JSON Payload
 headers = {
     'Content-Type': "application/json",
     'X-locale': "en_US",
     'Authorization': "Bearer "
     }
 
-response = requests.post(ups_url, data=payload, headers=headers)
+response = requests.post(ups_url, data=ups_payload, headers=headers)
 
 print(response.text)
