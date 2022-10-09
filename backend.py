6adcf0be698273de4c6af524a666c7a99255b9a5
Bigjango13 <bigjango13@gmail.com>
2022-10-09 00:01:46 -0400

    Small changes and minor checks

--- backend.py
+++ backend.py
@@ -3,9 +3,12 @@
 
 import requests
 import shippo
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
-def get_rates(api_key: str, address_from: dict, address_to: dict, parcel: dict) -> list[shippo.resource.ShippoObject]:
+
+def get_rates(
+    api_key: str, address_from: dict, address_to: dict, parcel: dict
+) -> list[shippo.resource.ShippoObject]:
     """
     api_key should be a shippo API key.
     address_from and address_to should be as documented here: https://goshippo.com/docs/reference#addresses
@@ -20,7 +23,7 @@ def get_rates(api_key: str, address_from: dict, address_to: dict, parcel: dict) 
         address_from=address_from,
         address_to=address_to,
         parcels=[parcel],
-        asynchronous=False
+        asynchronous=False,
     )
 
     # Rates are stored in the `rates` array
@@ -29,25 +32,42 @@ def get_rates(api_key: str, address_from: dict, address_to: dict, parcel: dict) 
 
     return rates
 
+
 app = Flask(__name__)
 
-@app.route('/rates', methods=['POST'])
 
+@app.route("/rates", methods=["POST", "GET"])
 def rates():
     # Get data from request
     data = request.get_json()
+    if data == None:
+        return "{}"
+
+    for arg in [
+        data["api_key"],
+        data["address_from"],
+        data["address_to"],
+        data["parcel"],
+    ]:
+        if arg == None:
+            return "{}"
+        print(arg)
 
     # Get rates
-    rates = get_rates(data['api_key'], data['address_from'], data['address_to'], data['parcel'])
+    rates = get_rates(
+        data["api_key"], data["address_from"], data["address_to"], data["parcel"]
+    )
 
     # Return rates
     return jsonify(rates)
 
+
 # 404 handling
 @app.errorhandler(404)
-def not_found(e):
-    # Redirect the gh repo
-  return '<meta http-equiv="Refresh" content="0; url=\'https://github.com/NENB-Hackathons/trihacks\'" />'
+def not_found(e=None):
+    # Redirect the github repo
+    return '<meta http-equiv="Refresh" content="0; url=\'https://github.com/NENB-Hackathons/trihacks\'" />'
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
